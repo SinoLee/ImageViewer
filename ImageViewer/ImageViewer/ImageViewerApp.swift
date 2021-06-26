de0eeb4875f1f474861d3621fa1ed42945822e45
Taeyoun Lee <sino.lty@gmail.com>
2021-06-26 14:17:54 +0900

    Create Project

---
+++ ImageViewer/ImageViewer/ImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerApp.swift
+//  ImageViewer
+//
+//  Created by Taeyoun Lee on 2021/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
